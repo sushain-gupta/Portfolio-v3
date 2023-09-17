f609e0e3244aa564bcc38912d67fa473faeed425
sushain-gupta <sushaingupta135@gmail.com>
2023-09-17 19:15:05 +0530

    Fix: project card option slider

--- src/Global/ProjectCard.jsx
+++ src/Global/ProjectCard.jsx
@@ -21,8 +21,8 @@ const ProjectCard = ({ project, styles, initials }) => {
 						transition={{
 							type: "spring",
 						}}
-						onMouseEnter={() => setOpen(true)}
-						onMouseLeave={() => setOpen(false)}
+						onMouseOver={() => open ? setOpen(false) : setOpen(true)}
+						onMouseOut={() => setOpen(false)}
 						className="relative flex flex-col justify-center mx-auto overflow-hidden bg-gray-100 rounded-2xl group sm:w-2/3 lg:w-full"
 					>
 						<div className="z-10 flex items-center h-full px-6 py-2 w-86">
@@ -84,67 +84,68 @@ const ProjectCard = ({ project, styles, initials }) => {
 					</motion.div >
 
 					:
-					(<div
-						style={styles}
-						onMouseEnter={() => setOpen(true)}
-						onMouseLeave={() => setOpen(false)}
-						className="relative flex flex-col justify-center mx-auto overflow-hidden bg-gray-100 rounded-2xl group sm:w-2/3 lg:w-full"
-					>
-						<div className="z-10 flex items-center h-full px-6 py-2 w-86">
-							<div>
-								<img
-									className="object-contain w-full h-full m-auto rounded-xl drop-shadow-md"
-									src={project.ImageUrl}
-									alt={project.Title + " image"}
-								/>
-							</div>
-						</div>
-
-						<div className="flex flex-col justify-between px-6 pb-4 text-sm">
-							<p className="mb-2 text-lg font-bold">{project.Title}</p>
-
-							<div>
-								<p>
-									<span className="font-bold">Techstack</span> - {project.Techstack}
-								</p>
-								<p>
-									<span className="font-bold">Libraries</span> - {project.Library}
-								</p>
-							</div>
-						</div>
-
+					(
 						<div
-							className={`${open ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
-								} absolute flex top-0 h-full w-full my-auto bottom-0  group-hover: z-20 right-0 bg-sky-900/70 duration-700 rounded-2xl`}
+							style={styles}
+							onMouseOver={() => open ? setOpen(false) : setOpen(true)}
+							onMouseOut={() => setOpen(false)}
+							className="relative flex flex-col justify-center mx-auto overflow-hidden bg-gray-100 rounded-2xl group sm:w-2/3 lg:w-full"
 						>
-							<div className="flex justify-between gap-6 m-auto text-2xl w-max h-max">
-								<Link
-									to={project.Github}
-									className="p-2 duration-500 rounded-full bg-slate-50 hover:rotate-6"
-								>
-									<AiFillGithub />
-								</Link>
-
-								<Link
-									to={project.ProjectLink}
-									className="p-2 duration-500 rounded-full bg-slate-50 hover:rotate-6"
-								>
-									<HiOutlineExternalLink />
-								</Link>
-
-								<button
-									onClick={() => setOpen(false)}
-									className="p-2 duration-500 rounded-full bg-slate-50 hover:rotate-90"
-								>
-									<IoIosClose />
-								</button>
+							<div className="z-10 flex items-center h-full px-6 py-2 w-86">
+								<div>
+									<img
+										className="object-contain w-full h-full m-auto rounded-xl drop-shadow-md"
+										src={project.ImageUrl}
+										alt={project.Title + " image"}
+									/>
+								</div>
 							</div>
-						</div>
 
-						<div
-							className={`top-0 absolute w-full h-1/2 bg-gradient-to-br from-gray-700 via-gray-900 to-black`}
-						></div>
-					</div>)
+							<div className="flex flex-col justify-between px-6 pb-4 text-sm">
+								<p className="mb-2 text-lg font-bold">{project.Title}</p>
+
+								<div>
+									<p>
+										<span className="font-bold">Techstack</span> - {project.Techstack}
+									</p>
+									<p>
+										<span className="font-bold">Libraries</span> - {project.Library}
+									</p>
+								</div>
+							</div>
+
+							<div
+								className={`${open ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+									} absolute flex top-0 h-full w-full my-auto bottom-0  group-hover: z-20 right-0 bg-sky-900/70 duration-700 rounded-2xl`}
+							>
+								<div className="flex justify-between gap-6 m-auto text-2xl w-max h-max">
+									<Link
+										to={project.Github}
+										className="p-2 duration-500 rounded-full bg-slate-50 hover:rotate-6"
+									>
+										<AiFillGithub />
+									</Link>
+
+									<Link
+										to={project.ProjectLink}
+										className="p-2 duration-500 rounded-full bg-slate-50 hover:rotate-6"
+									>
+										<HiOutlineExternalLink />
+									</Link>
+
+									<button
+										onClick={() => setOpen(false)}
+										className="p-2 duration-500 rounded-full bg-slate-50 hover:rotate-90"
+									>
+										<IoIosClose />
+									</button>
+								</div>
+							</div>
+
+							<div
+								className={`top-0 absolute w-full h-1/2 bg-gradient-to-br from-gray-700 via-gray-900 to-black`}
+							></div>
+						</div>)
 			}
 		</>
 
