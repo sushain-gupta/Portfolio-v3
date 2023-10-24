3f5f515c072a70ea0d7fe0f43ac8fc388f9a0b06
sushain-gupta <sushaingupta135@gmail.com>
2023-10-25 00:54:21 +0530

    Changes: experience section

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,5 +1,17 @@
 export const experience = [
 	{
+		companyName: "Stratmark Consulting",
+		companyLogo:
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1696175985/Portfolio/exp/Untitled_design_6_d8u4nv.png",
+		jobRole: "Freelance",
+		duration: "Aug 2023 - Present",
+		techstack: ["ReactJs", "Tailwind CSS", "MUI", "Redux Toolkit"],
+		description: [
+			"Developed high-quality and fully-functional Web App Mockup as per the client requirements.",
+			"Implemented PWA using ReactJs, Tailwind CSS, MUI, Redux Toolkit and more other technologies.",
+		],
+	},
+	{
 		companyName: "Codebell Technologies",
 		companyLogo:
 			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1694527181/Portfolio/exp/1686382464747_vc7glk.jpg",
@@ -9,22 +21,6 @@ export const experience = [
 		description: [
 			"Implemented day to day design changes from Figma to live website.",
 			"Developed high-quality, responsive, and userfriendly website.",
-		],
-	},
-	{
-		companyName: "Stratmark Consulting",
-		companyLogo:
-			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1696175985/Portfolio/exp/Untitled_design_6_d8u4nv.png",
-		jobRole: "Freelance",
-		duration: "Aug 2023 - Present",
-		techstack: [
-			"ReactJs",
-			"Tailwind CSS",
-			"MUI",
-			"Redux Toolkit"],
-		description: [
-			"Developed high-quality and fully-functional Web App Mockup as per the client requirements.",
-			"Implemented PWA using ReactJs, Tailwind CSS, MUI, Redux Toolkit and more other technologies.",
 		],
 	},
 ];
