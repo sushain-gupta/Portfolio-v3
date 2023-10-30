82ff793affbfb9071ca1bf724a4c070f275d732e
sushain-gupta <sushaingupta135@gmail.com>
2023-10-30 11:37:00 +0530

    Added skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -25,9 +25,40 @@ export const skills = [
 	},
 	{
 		skillIcon:
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1698643899/Portfolio/skills/icons8-nodejs_lmjqiz.svg",
+		skillName: "NodeJs",
+	},
+	{
+		skillIcon:
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1698644086/Portfolio/skills/icons8-express-js_aucg1o.svg",
+		skillName: "ExpressJs",
+	},
+	{
+		skillIcon:
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1698644406/Portfolio/skills/mongodb_original_wordmark_logo_icon_146425_qknszu.svg",
+		skillName: "MongoDB",
+	},
+	{
+		skillIcon:
 			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1694530093/Portfolio/skills/pwa-icon_kqwk0u.svg",
 		skillName: "Web App",
 		major: true,
+	},
+	{
+		skillIcon:
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1674668115/Portfolio/skills/firebase_dwkgf4.png",
+		skillName: "Firebase",
+		major: true,
+	},
+	{
+		skillIcon:
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1698645002/Portfolio/skills/svgexport-7_vhqxeb.svg",
+		skillName: "AWS Ec2",
+	},
+	{
+		skillIcon:
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1698644998/Portfolio/skills/svgexport-9_qst9qj.svg",
+		skillName: "Ec2 Auto Scaling",
 	},
 	{
 		skillIcon:
@@ -43,12 +74,6 @@ export const skills = [
 	},
 	{
 		skillIcon:
-			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1674668115/Portfolio/skills/firebase_dwkgf4.png",
-		skillName: "Firebase",
-		major: true,
-	},
-	{
-		skillIcon:
 			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1674668115/Portfolio/skills/Bootstrap_mnezkw.svg",
 		skillName: "Bootstrap",
 	},
@@ -59,25 +84,14 @@ export const skills = [
 	},
 	{
 		skillIcon:
-			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1674668116/Portfolio/skills/python_drwvw3.svg",
-		skillName: "Python",
+			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1698644156/Portfolio/skills/icons8-git_t7nlho.svg",
+		skillName: "Git",
+		major: true,
 	},
 	{
 		skillIcon:
 			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1678096601/Portfolio/skills/github-logo_s0trsu.png",
 		skillName: "GitHub",
-		major: true,
-	},
-	{
-		skillIcon:
-			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1674668117/Portfolio/skills/netlify_abvuz1.svg",
-		skillName: "Netlify",
-		major: true,
-	},
-	{
-		skillIcon:
-			"https://res.cloudinary.com/dkezwrb3a/image/upload/v1694444099/Portfolio/skills/Vercel_favicon_j5vckf.svg",
-		skillName: "Vercel",
 		major: true,
 	},
 ];
