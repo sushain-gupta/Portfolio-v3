b0bc0a6a8fee2bb7e3fbdef6c3f4fb4fa687ed66
sushain-gupta <sushaingupta135@gmail.com>
2024-06-14 01:51:53 +0530

    Fix: header issue in project section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import Footer from "./Global/Footer";
 import { Route, Routes } from "react-router-dom";
 import ProjectLibrary from "./Pages/Projects";
 import Main from "./Pages/Main";
-import Header from "./Global/Header";
 
 const App = () => {
 	return (
 		<div className="relative text-gray-700">
-			<Header />
-
 			<div className="absolute bg-homeBg bg-no-repeat bg-cover h-[100dvh] w-full -z-20"></div>
 
 			<div className="absolute h-[120dvh] w-full -z-10 bg-gradient-to-b from-white/90 to-white"></div>
