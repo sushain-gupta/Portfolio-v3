b03d8c27be2203256b2530af23b9282974acdae5
Sushain Gupta <sushain.work@gmail.com>
2025-08-14 15:27:13 +1000

    Add: experience description

--- src/data/experience.js
+++ src/data/experience.js
@@ -5,10 +5,15 @@ export const experience = [
 			"https://res.cloudinary.com/sushain/image/upload/v1735736179/Portfolio/exp/359453892_597778089149141_4521339302675636379_n_khryjs.jpg",
 		jobRole: "ASE",
 		duration: "Aug 2024 - Present",
-		techstack: null,
+		techstack: ["Harness", "Artifactory", "Snowflake", "AWS", "EKS", "Apache Nifi", "Apache Airflow", "Bash scripting", "Linux", "Git", "PL/SQL", "Oracle Apex", "Python"],
 		description: [
-			"Focusing on enhancing technical skills",
-			"Preparing for upcoming projects and industry experience",
+			"Part of a migration project moving existing mainframe processes to the cloud, utilising Kubernetes, Harness, JFrog, Bash, and AWS to modernise infrastructure and streamline deployments.",
+			"Designed and maintained Apache NiFi data pipelines, pulling data from Amazon S3 or Salesforce, and transforming it for downstream analytical consumption in Snowflake.",
+			"Worked as a Core Developer, responsible for implementing and optimising ETL workflows using SQL, Python, Apache NiFi, and Snowflake, ensuring seamless data transformation and loading.",
+			"Created automated Python scripts that significantly reduced job completion time and improved overall workflow efficiency.",
+			"Developed scripts for automated execution using Apache Airflow, enabling efficient data processing and filtration for analytical use.",
+			"Served as a client-facing developer, actively engaging with stakeholders to gather requirements and deliver data-driven solutions tailored to business needs.",
+			"Completed training in Oracle PL/SQL, Oracle APEX, Snowflake, and Python, gaining a strong foundation in database development and backend programming."
 		],
 	},
 	{
